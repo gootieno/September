c71f0a08cf121f4e984aa0c5f9c595b02d7406a8
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2022-11-16 14:07:33 -0800

    fetch final solution

--- Week8/Day3/Solutions/04-fetch/fetch-requests.js
+++ Week8/Day3/Solutions/04-fetch/fetch-requests.js
@@ -17,6 +17,43 @@ fetch("/products")
     console.log("response text after .then ", data);
   });
 
+const getProducts = async () => {
+  const response = await fetch("/products");
+
+  console.log("response ", response);
+
+  console.log("response ok? ", response.ok);
+
+  if (response.ok) {
+    console.log("response status ", response.status);
+    console.log("response headers ", response.headers.get("Content-Type"));
+
+    // promise pending
+    // console.log("response text ", response.text());
+
+    // promise fulfilled
+    const data = await response.text();
+    console.log("response data ", data);
+  }
+  // error handle if !response.ok
+};
+
+getProducts();
+
+fetch("/products")
+  .then(async (response) => {
+    console.log("response ", response);
+
+    console.log("response status ", response.status);
+    console.log("response ok? ", response.ok);
+    console.log("response headers ", response.headers.get("Content-Type"));
+
+    //   console.log("response text ", response.text());
+    // response.text().then(console.log);
+    // console.log('await version text ', await response.text())
+  })
+  .then((responseData) => console.log("response text ", responseData));
+
 /* ====== 2. Print true if the status of the response was successful ====== */
 
 // Your code here
